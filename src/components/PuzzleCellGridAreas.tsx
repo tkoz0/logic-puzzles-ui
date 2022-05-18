f251c550d29e2b5cba69314c60ad6b6fcb0f40cd
tkoz0 <null>
2022-05-18 03:05:36 -0400

    component for handling borders for areas

---
+++ src/components/PuzzleCellGridAreas.tsx
@@ -0,0 +1,62 @@
+import {forwardRef, useImperativeHandle, useRef, useState} from "react";
+import PuzzleCellGrid, {PuzzleCellGridHandle, PuzzleCellGridProps} from "./PuzzleCellGrid";
+
+// border widths
+const B_THIN = 1;
+const B_THICK = 3;
+
+interface PuzzleCellGridAreasHandle extends PuzzleCellGridHandle {
+    getAreas: () => number[][],
+    setAreas: (newAreas: number[][]) => void
+}
+
+interface Props extends PuzzleCellGridProps {
+    // extension to PuzzleCellGrid
+    areas: number[][],
+}
+
+/**
+ * Extends PuzzleGrid by supporting areas and handling drawing the borders.
+ */
+const PuzzleCellGridAreas = forwardRef<PuzzleCellGridAreasHandle,Props>((props,ref) => {
+    console.assert(props.areas.length === props.data.length
+        && props.areas[0].length === props.data[0].length,
+        "PuzzleCellGridAreas: mismatched areas and data sizes");
+    const R = props.data.length;
+    const C = props.data[0].length;
+    const gridRef = useRef<PuzzleCellGridHandle>(null);
+    const [areas,setAreas] = useState(props.areas);
+
+    useImperativeHandle(ref, () => ({
+        ...(gridRef as unknown as PuzzleCellGridHandle),
+        getAreas: () => areas,
+        setAreas: newAreas => {
+            console.assert(newAreas.length === areas.length
+                && newAreas[0].length === areas[0].length,
+                "PuzzleCellGridAreas.setAreas(): incorrect areas size");
+            setAreas(newAreas);
+        }
+    }));
+
+    return (
+        <PuzzleCellGrid ref={gridRef}
+            {...props}
+            styles={areas.map((row,r) => row.map((area,c) => {
+                // thick border around edges and where area number changes
+                const bl = (c === 0 || area !== areas[r][c-1]) ? B_THICK : B_THIN;
+                const br = (c+1 === C || area !== areas[r][c+1]) ? B_THICK : B_THIN;
+                const bt = (r === 0 || area !== areas[r-1][c]) ? B_THICK : B_THIN;
+                const bb = (r+1 === R || area !== areas[r+1][c]) ? B_THICK : B_THIN;
+                return {
+                    borderLeft: bl+"px solid black",
+                    borderRight: br+"px solid black",
+                    borderTop: bt+"px solid black",
+                    borderBottom: bb+"px solid black",
+                    ...props.styles[r][c]
+                }
+            }))}
+        />
+    );
+});
+
+export default PuzzleCellGridAreas;
