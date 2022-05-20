e9f2114048fffe6abdeb40ec02774e254e1f0403
tkoz0 <tkozlowski1999@gmail.com>
2022-05-20 02:35:35 -0400

    some adjustments for now

--- src/components/PuzzleCellGridAreas.tsx
+++ src/components/PuzzleCellGridAreas.tsx
@@ -6,11 +6,9 @@ const B_THIN = 1;
 const B_THICK = 3;
 
 interface PuzzleCellGridAreasHandle extends PuzzleCellGridHandle {
-    getAreas: () => number[][],
-    setAreas: (newAreas: number[][]) => void
 }
 
-interface Props<T> extends PuzzleCellGridProps<T> {
+interface Props extends PuzzleCellGridProps {
     // extension to PuzzleCellGrid
     areas: number[][],
     borderThick?: string,
@@ -20,53 +18,42 @@ interface Props<T> extends PuzzleCellGridProps<T> {
 /**
  * Extends PuzzleGrid by supporting areas and handling drawing the borders.
  */
-const PuzzleCellGridAreas = <T extends unknown>(props: Props<T>,
+const PuzzleCellGridAreas = (props: Props,
         ref: ForwardedRef<PuzzleCellGridAreasHandle>) => {
-    console.assert(props.areas.length === props.data.length
-        && props.areas[0].length === props.data[0].length,
-        "PuzzleCellGridAreas: mismatched areas and data sizes");
-    const R = props.data.length;
-    const C = props.data[0].length;
-    const gridRef = useRef<PuzzleCellGridHandle>(null);
+    const R = props.rows;
+    const C = props.cols;
+    const iref = useRef<PuzzleCellGridHandle>(null);
     const [areas,setAreas] = useState(props.areas);
 
     useImperativeHandle(ref, () => ({
         // inherited
-        getFullData: () => gridRef.current!.getFullData(),
-        getFullStyles: () => gridRef.current!.getFullStyles(),
-        updateData: () => gridRef.current!.updateData(),
-        updateStyles: () => gridRef.current!.updateStyles(),
-        getSelected: () => gridRef.current!.getSelected(),
-        setSelected: (r,c) => gridRef.current!.setSelected(r,c),
+        getFullData: () => iref.current!.getFullData(),
+        getFullStyles: () => iref.current!.getFullStyles(),
+        updateData: () => iref.current!.updateData(),
+        updateStyles: () => iref.current!.updateStyles(),
+        getSelected: () => iref.current!.getSelected(),
+        setSelected: (r,c) => iref.current!.setSelected(r,c),
         // extension
-        getAreas: () => areas,
-        setAreas: newAreas => {
-            console.assert(newAreas.length === areas.length
-                && newAreas[0].length === areas[0].length,
-                "PuzzleCellGridAreas.setAreas(): incorrect areas size");
-            setAreas(newAreas);
-        }
     }));
 
     return (
-        <PuzzleCellGrid ref={gridRef}
+        <PuzzleCellGrid ref={iref}
             {...props}
-            styles={areas.map((row,r) => row.map((area,c) => {
-                // thick border around edges and where area number changes
+            getStyle={(r,c) => {
                 const bthick = props.borderThick ?? B_THICK+"px solid black";
                 const bthin  = props.borderThin  ?? B_THIN +"px solid black";
-                const bl = (c === 0   || area !== areas[r][c-1]) ? bthick : bthin;
-                const br = (c+1 === C || area !== areas[r][c+1]) ? bthick : bthin;
-                const bt = (r === 0   || area !== areas[r-1][c]) ? bthick : bthin;
-                const bb = (r+1 === R || area !== areas[r+1][c]) ? bthick : bthin;
+                const bl = (c === 0   || areas[r][c] !== areas[r][c-1]) ? bthick : bthin;
+                const br = (c+1 === C || areas[r][c] !== areas[r][c+1]) ? bthick : bthin;
+                const bt = (r === 0   || areas[r][c] !== areas[r-1][c]) ? bthick : bthin;
+                const bb = (r+1 === R || areas[r][c] !== areas[r+1][c]) ? bthick : bthin;
                 return {
                     borderLeft:   bl,
                     borderRight:  br,
                     borderTop:    bt,
                     borderBottom: bb,
-                    ...props.styles[r][c]
-                }
-            }))}
+                    ...props.getStyle(r,c)
+                };
+            }}
         />
     );
 };
